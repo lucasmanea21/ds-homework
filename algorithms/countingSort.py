87381090a2d577b74ae0b70c3f6d079ac414d26e
lucasmanea21 <lucasmanea@yahoo.com>
2025-04-06 20:41:51 +0300

    radix for hexa

--- algorithms/countingSort.py
+++ algorithms/countingSort.py
@@ -1,20 +1,20 @@
-def countingSort(arr, exp):
+def countingSort(arr, shift, mask):
     if not arr:
         return arr
     
     n = len(arr)
     output = [0] * n
-    count = [0]*10
+    count = [0]* (mask + 1)
     
     for num in arr:
-        index = int((num // exp) % 10)
+        index = (num >> shift) & mask
         count[index] += 1
-        
-    for i in range(1, 10):
-        count[i] += count[i-1]
-    
+
+    for i in range(1, len(count)):
+        count[i] += count[i - 1]
+
     for i in reversed(range(n)):
-        index = int((arr[i] // exp) % 10)
+        index = (arr[i] >> shift) & mask
         count[index] -= 1
         output[count[index]] = arr[i]
 
